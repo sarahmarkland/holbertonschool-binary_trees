3c90b621fd56aaf73764d737ed644c67998c27ff
sarahmarkland <sarah.markland@icloud.com>
2023-04-21 14:05:49 -0700

    task 1 commit 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,13 +7,25 @@
  *
  * Return: pointer to the created node, or NULL on failure or if parent is NULL
  */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *newNode;
-    
-    newNode = malloc(sizeof(binary_tree_t));
-    if (parent == NULL)
-        return (NULL);
-    
-    malloc
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+
+	new_node = binary_tree_node(parent, value);
+
+	if (new_node == NULL)
+		return (NULL);
+
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
+
+	parent->left = new_node;
+
+	return (new_node);
 }
