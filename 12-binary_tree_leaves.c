c9b24b36d2fb61b67a26ef9bd645055bb1dd2143
sarahmarkland <sarah.markland@icloud.com>
2023-04-22 17:07:08 -0700

    t 12 c 2

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,16 +7,16 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t leaves = 0;
+	size_t leaves = 0;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    if (tree->left == NULL && tree->right == NULL)
-        leaves++;
+	if (tree->left == NULL && tree->right == NULL)
+		leaves++;
 
-    leaves += binary_tree_leaves(tree->left);
-    leaves += binary_tree_leaves(tree->right);
+	leaves += binary_tree_leaves(tree->left);
+	leaves += binary_tree_leaves(tree->right);
 
-    return (leaves);
+	return (leaves);
 }
