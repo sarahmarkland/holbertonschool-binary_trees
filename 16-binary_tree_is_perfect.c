6fb5d10ca5cca656640d4116fc7b884cecd45477
sarahmarkland <sarah.markland@icloud.com>
2023-04-22 20:52:37 -0700

    task 16 commit add height func to 16, instead of adding to comp code

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,25 +1,45 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: height of a binary tree
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left, right;
+
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+	left = binary_tree_height(tree->left);
+	right = binary_tree_height(tree->right);
+	if (left > right)
+		return (left + 1);
+	else
+		return (right + 1);
+}
+/**
  * binary_tree_is_perfect - checks if a binary tree is perfect
  * @tree: pointer to the root node of the tree to check
  * Return: 1 if tree is perfect, 0 otherwise
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-    int left, right;
+	int left, right;
 
-    if (tree == NULL)
-        return (0);
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
-    if (tree->left == NULL || tree->right == NULL)
-        return (0);
-    left = binary_tree_is_perfect(tree->left);
-    right = binary_tree_is_perfect(tree->right);
-    if (left == 0 || right == 0)
-        return (0);
-    if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-        return (1);
-    return (0);
+	if (tree == NULL)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	if (tree->left == NULL || tree->right == NULL)
+		return (0);
+	left = binary_tree_is_perfect(tree->left);
+	right = binary_tree_is_perfect(tree->right);
+	if (left == 0 || right == 0)
+		return (0);
+	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+		return (1);
+	return (0);
 }
